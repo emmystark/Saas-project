7fba40c4585828a2a1a5d189208326e451d2f554
emmystark <nkereuwememmanuel03@gmail.com>
2025-12-14 10:22:57 +0100

    implemented clerk managemaent-system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-import Link from "next/link";
 
-import { LatestPost } from "@/app/_components/post";
-import { api, HydrateClient } from "@/trpc/server";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
 
-  void api.post.getLatest.prefetch();
 
   return (
-    <h1 className="text-red-700">Hello, World</h1>
+    <Button>Hello World</Button>
   );
 }
